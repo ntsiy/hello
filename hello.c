692e3006b133fdae7a4d0293bf6ce9f6a922b361
ntsiy <112456068+ntsiy@users.noreply.github.com>
2022-08-30 03:40:54 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Nathan Siy
+ *         nsiy2@unl.edu
+ * Date: 8/23/2022
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nathan Siy");
+
+  return 0;
+}
